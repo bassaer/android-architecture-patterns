e17a729fabf164d8f6c92974fb7710b4bb7c1d37
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-03-15 22:01:23 +0900

    initial commit

---
+++ app/src/main/java/com/github/bassaer/android_architecture_patterns/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.bassaer.android_architecture_patterns
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
