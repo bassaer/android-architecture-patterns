29590928fd5bc8fbd5e03b7c5812995e8da80be6
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-03-19 23:22:24 +0900

    fix BaseView

--- todo-mvp/src/main/java/com/github/bassaer/todomvp/BaseView.kt
+++ todo-mvp/src/main/java/com/github/bassaer/todomvp/BaseView.kt
@@ -1,8 +1,9 @@
 package com.github.bassaer.todomvp
 
 /**
+ * BaseView
  * Created by nakayama on 2018/03/18.
  */
-public interface BaseView<T> {
-    fun setPresenter(presenter: T)
+interface BaseView<T> {
+    var presenter: T
 }
